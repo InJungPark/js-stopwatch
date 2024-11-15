c654f7d992b0b5ec19d9a1f20ce14b881a92d8f6
박인정(Jay) <86592396+InJungPark@users.noreply.github.com>
2024-11-15 23:48:44 +0900

    feat: completed digital clock in JS, final touches done

--- app.js
+++ app.js
@@ -1,81 +1,222 @@
-const addTrailingZero = (number) => {
-  return number < 10 ? `0${number}` : number;
-};
-
-/**
- * Clock = Timer
- */
+// Clock
 const updateTime = () => {
-  // 함수명이 명사나 형용사로 끝내기
-  // 값이 고정되기 때문에 const를 사용함
   const time = new Date();
-
-  // 값이 변화되기 때문에 let을 사용함
   let hours = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
-  let am = hours >= 12 ? "PM" : "AM"; // AM에서 PM으로 넘어가는 것( AM > PM )
-  let pm = hours >= 12 ? "AM" : "PM"; // PM에서 AM으로 넘어가는 것( PM > AM )
+  let ampm = hours >= 12 ? "PM" : "AM";
+  let otherampm = hours >= 12 ? "AM" : "PM";
 
-  hours = hours % 12 || 12; // converting을 함( 24시간을 12시간으로 전환시켜 줌 )
-
-  /*
-   * addTrailingZero : 숫자에 소수점 이하 자릿수를 맞추기 위해 0을 추가해주는 함수
-   * ( 일의 자리수 부터 있는 0의 갯수 )
-   */
+  hours = hours % 12 || 12;
   hours = addTrailingZero(hours);
   minutes = addTrailingZero(minutes);
   seconds = addTrailingZero(seconds);
 
-  // Ajax 사용하여 해당 html 선언하기 (편의성으로 사용)
   $("#hour").html(hours);
   $("#min").html(minutes);
   $("#sec").html(seconds);
-  $("#am").html(am);
-  $("#pm").html(pm);
-
-  // getElemetById를 사용하여 innerHTEML을 할당하여 사용
-  // document.getElementById("hour").innerHTML = hours;
-  // document.getElementById("min").innerHTML = minutes;
-  // document.getElementById("sec").innerHTML = seconds;
-  // document.getElementById("am").innerHTML = am;
-  // document.getElementById("pm").innerHTML = pm;
+  $("#ampm").html(ampm);
+  $("#other-ampm").html(otherampm);
 };
 
 updateTime();
-setInterval(updateTime, 1000); // 시간을 매일 초단위로 업데이트( 1000은 1초 )
+setInterval(updateTime, 1000);
 
-/**
- * StopWatch
- */
-let stopWatchHour = 0,
-  stopWatchMinutes = 0,
-  stopWatchSecond = 0,
-  stopWatchMillisecond = 0,
-  stopWatchRunning = false,
+$("#stopwatch-btn").click(function () {
+  $(".main-container > div").slideUp();
+  $(".stopwatch").slideDown();
+  $(".type").html("Stopwatch");
+});
+
+$("#timer-btn").click(function () {
+  $(".main-container > div").slideUp();
+  $(".timer").slideDown();
+  $(".type").html("Timer");
+});
+
+$(".back-btn").click(function () {
+  $(".main-container > div").slideUp();
+  $(".clock").slideDown();
+  $(".type").html("Clock");
+});
+
+let stopwatchHours = 0,
+  stopwatchMinutes = 0,
+  stopwatchSeconds = 0,
+  stopwatchMilliSeconds = 0,
+  stopwatchRunning = false,
   laps = 0,
-  StopWatchInterval;
+  stopwatchInterval;
 
-const stopwatch = () => {
-  stopWatchMillisecond++;
-
-  if (stopWatchMillisecond === 100) {
-    stopWatchSecond++;
-    stopWatchMillisecond = 0;
+function stopwatch() {
+  stopwatchMilliSeconds++;
+  if (stopwatchMilliSeconds === 100) {
+    stopwatchMilliSeconds = 0;
+    stopwatchSeconds++;
+  }
+  if (stopwatchSeconds === 60) {
+    stopwatchSeconds = 0;
+    stopwatchMinutes++;
+  }
+  if (stopwatchMinutes === 60) {
+    stopwatchMinutes = 0;
+    stopwatchHours++;
   }
 
-  if (stopWatchSecond === 60) {
-    stopWatchMinutes++;
-    stopWatchSecond = 0;
+  $("#stopwatch-hour").html(addTrailingZero(stopwatchHours));
+  $("#stopwatch-min").html(addTrailingZero(stopwatchMinutes));
+  $("#stopwatch-sec").html(addTrailingZero(stopwatchSeconds));
+  $("#stopwatch-ms").html(addTrailingZero(stopwatchMilliSeconds));
+}
+
+function startStopwatch() {
+  if (!stopwatchRunning) {
+    stopwatchInterval = setInterval(stopwatch, 10);
+    stopwatchRunning = true;
+  }
+}
+
+function stopStopwatch() {
+  clearInterval(stopwatchInterval);
+  stopwatchRunning = false;
+}
+
+function resetStopwatch() {
+  clearInterval(stopwatchInterval);
+  stopwatchRunning = false;
+  stopwatchHours = 0;
+  stopwatchMinutes = 0;
+  stopwatchSeconds = 0;
+  stopwatchMilliSeconds = 0;
+  laps = 0;
+  $("#stopwatch-hour").html("00");
+  $("#stopwatch-min").html("00");
+  $("#stopwatch-sec").html("00");
+  $("#stopwatch-ms").html("00");
+}
+
+$(".start-stopwatch").click(function () {
+  startStopwatch();
+  $(".start-stopwatch").hide();
+  $(".lap-stopwatch").show();
+});
+
+$(".lap-stopwatch").click(function () {
+  laps++;
+  $(".lap").removeClass("active");
+  $(".laps").prepend(
+    ` <div class="lap active">
+      <p>Lap ${laps}</p>
+      <p>
+        ${addTrailingZero(stopwatchHours)} : ${addTrailingZero(stopwatchMinutes)} : ${addTrailingZero(
+      stopwatchSeconds
+    )} : ${addTrailingZero(stopwatchMilliSeconds)}
+      </p>
+    </div>
+   `
+  );
+});
+
+$(".reset-stopwatch").click(function () {
+  resetStopwatch();
+  $(".start-stopwatch").show();
+  $(".lap-stopwatch").hide();
+  $(".laps").html("");
+});
+
+function addTrailingZero(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+let time = 0,
+  timerHours = 0,
+  timerMinutes = 0,
+  timerSeconds = 0,
+  timerMilliseconds = 0,
+  timerRunning = false,
+  timerInterval;
+
+function getTime() {
+  time = prompt("Enter time in minutes");
+  time = time * 60;
+  setTime();
+}
+function setTime() {
+  timerHours = Math.floor(time / 3600);
+  timerMinutes = Math.floor((time % 3600) / 60);
+  timerSeconds = Math.floor(time % 60);
+  timerMilliseconds = 0;
+
+  $("#timer-hour").html(addTrailingZero(timerHours));
+  $("#timer-min").html(addTrailingZero(timerMinutes));
+  $("#timer-sec").html(addTrailingZero(timerSeconds));
+  $("#timer-ms").html(addTrailingZero(timerMilliseconds));
+}
+
+function timer() {
+  timerMilliseconds--;
+  if (timerMilliseconds === -1) {
+    timerMilliseconds = 99;
+    timerSeconds--;
+  }
+  if (timerSeconds === -1) {
+    timerSeconds = 59;
+    timerMinutes--;
+  }
+  if (timerMinutes === -1) {
+    timerMinutes = 59;
+    timerHours--;
   }
 
-  if (stopWatchMinutes === 60) {
-    stopWatchHour++;
-    stopWatchMinutes = 0;
-  }
+  $("#timer-hour").html(addTrailingZero(timerHours));
+  $("#timer-min").html(addTrailingZero(timerMinutes));
+  $("#timer-sec").html(addTrailingZero(timerSeconds));
+  $("#timer-ms").html(addTrailingZero(timerMilliseconds));
 
-  $("#stopWatch-hour").html(addTrailingZero(stopWatchHour));
-  $("#stopWatch-min").html(addTrailingZero(stopWatchMinutes));
-  $("#stopWatch-sec").html(addTrailingZero(stopWatchSecond));
-  $("#stopWatch-ms").html(addTrailingZero(stopWatchMillisecond));
-};
+  timeUp();
+}
+
+function startTimer() {
+  if (timerHours === 0 && timerMinutes === 0 && timerSeconds === 0) {
+    getTime();
+  } else {
+    timerInterval = setInterval(timer, 10);
+    timerRunning = true;
+    $(".start-timer").hide();
+    $(".stop-timer").show();
+  }
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerRunning = false;
+  $(".start-timer").show();
+  $(".stop-timer").hide();
+}
+
+function resetTimer() {
+  stopTimer();
+  time = 0;
+  setTime();
+}
+
+function timeUp() {
+  if (timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 && timerMilliseconds === 0) {
+    stopTimer();
+    alert("Time's up!");
+
+    setTime();
+  }
+}
+
+$(".start-timer").click(startTimer);
+
+$(".stop-timer").click(stopTimer);
+
+$(".reset-timer").click(function () {
+  resetTimer();
+  if (!timerRunning) {
+    $(".start-timer").show();
+    $(".stop-timer").hide();
+  }
+});
